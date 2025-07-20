ffa5033f4b54e9065a1af4dac5597b9d127f1e7e
Higash37 <higashionna37@icloud.com>
2025-07-20 21:32:45 +0900

    vercelデプロイ

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -73,7 +73,7 @@ const theme = createTheme({
     },
     MuiSelect: {
       styleOverrides: {
-        root: {
+        select: {
           borderRadius: 10, // Selectコンポーネントの角丸
         },
       },
